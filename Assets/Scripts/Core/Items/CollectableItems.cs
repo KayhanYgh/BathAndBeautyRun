07eaadb5381cfbb8acd8188c4fbdd4c25d9fe9c1
amirhebadatiyaan <amirhosseinebadatian@gmail.com>
2021-08-08 22:21:30 +0430

    Bug fixed

--- Assets/Scripts/Core/Items/CollectableItems.cs
+++ Assets/Scripts/Core/Items/CollectableItems.cs
@@ -8,15 +8,17 @@ public class CollectableItems : MonoBehaviour
 
     public void Collect()
     {
+        onCollect?.Invoke();
+
         if (levelEffect > 0)
         {
             Score.ScoreManager.Instance.scoreText.text = (int.Parse(Score.ScoreManager.Instance.scoreText.text)+ levelEffect).ToString();
         }
 
+        Debug.Log($"Dirt level {levelEffect}");
         Player.Instance.dirtyLevel += levelEffect;
         Player.Instance.dirtyLevelSlider.value = Player.Instance.dirtyLevel;
 
-        onCollect?.Invoke();
 
         gameObject.GetComponent<BoxCollider>().enabled = false;
     }
